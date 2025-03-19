2dec0bca0cb19cf721c169329227973ca784c6ae
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-03-19 21:25:22 +0100

    Add MaxHeightConverter

---
+++ Chronomancer/Converters/MaxHeightConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Chronomancer
+{
+    public class MaxHeightConverter : IValueConverter
+    {
+        // Returns the product of @value = number of elements and @parameter = max height of one element
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int && parameter is double)
+            {
+                return (int)value * (double)parameter;
+            }
+            else
+            {
+                Console.WriteLine("ERROR: MaxHeightConverter called with incorrect type of value or parameter");
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
