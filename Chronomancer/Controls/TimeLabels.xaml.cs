5846e477f152fa9a7124d4c778f98c488bc24efc
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-04-01 14:08:30 +0200

    Create alpha-version of user control for time labels

---
+++ Chronomancer/Controls/TimeLabels.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Chronomancer
+{
+    public partial class TimeLabels : UserControl
+    {
+        public TimeLabels()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty RightTicksProperty = DependencyProperty.Register(
+            "RightTicks", typeof(bool), typeof(TimeLabels));
+        public static readonly DependencyProperty LeftTicksProperty = DependencyProperty.Register(
+            "LeftTicks", typeof(bool), typeof(TimeLabels));
+        public static readonly DependencyProperty TextAlignmentProperty = DependencyProperty.Register(
+            "TextAlignment", typeof(HorizontalAlignment), typeof(TimeLabels));
+        public required bool RightTicks { get; set; } = true;
+        public required bool LeftTicks { get; set; } = false;
+        public required HorizontalAlignment TextAlignment { get; set; } = HorizontalAlignment.Left;
+    }
+}
